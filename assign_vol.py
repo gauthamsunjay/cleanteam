397a26f32d4583ae6188d3354817a2bacbab813d
Vivek Bharadwaj <bvivek35@gmail.com>
2016-04-26 21:38:23 +0530

    add only few of dates for each co_ord instead of all days. Added logic to control end_date and no_of_days_before in generation of dates

--- assign_vol.py
+++ assign_vol.py
@@ -2,14 +2,16 @@
 
 import json
 import datetime
+import dateutil.parser
+import random
 
 import utils
 
 if __name__ == '__main__' :
 
     import sys
-    if len(sys.argv) != 5 :
-        print 'USAGE : ./pgm <json file of co_ords> <max val of garbage> <valid no of days before today for garbage> <json locations file>'
+    if len(sys.argv) != 6 :
+        print 'USAGE : ./pgm <json file of co_ords> <max val of garbage> <end date> <valid no of days before end date for garbage> <json locations file>'
         print 'EXAMPLE : ' + sys.argv[0] + ' co_ords 1000 150.5 20 res'
         sys.exit(1)
 
@@ -17,14 +19,31 @@ if __name__ == '__main__' :
     n_gen = len(co_ords_json)
 
     max_vol = float(sys.argv[2])
-    n_days_before = int(sys.argv[3])
+
+    end_date = dateutil.parser.parse(sys.argv[3])
+    n_days_before = int(sys.argv[4])
     tot = n_gen * n_days_before
     
     rand_vols = utils.gen_rand_vols(tot, max_vol)
 
-    rand_dates = utils.gen_rand_dates(tot, datetime.datetime.today(), n_days_before)
+    rand_dates = utils.gen_rand_dates(tot, end_date, n_days_before)
 
 
+    # create json result
+    res = []
+    idx = 0
+    for i in range(n_gen) :
+        reps = random.randint(0, 20)
+        print (i, reps)
+        for i in range(reps) :
+            tmp = {}
+            tmp['co_ord'] = co_ords_json[i]['co_ord']
+            tmp['volume'] = rand_vols[idx]
+            tmp['datetime'] = rand_dates[idx]
+            idx += 1
+            res.append(tmp)
+
+    """
     # create json result
     res = []
     for i in range(tot) :
@@ -34,6 +53,6 @@ if __name__ == '__main__' :
         tmp['volume'] = rand_vols[i]
         tmp['datetime'] = rand_dates[i]
         res.append(tmp)
-
-    outfile = sys.argv[4]
+    """
+    outfile = sys.argv[5]
     utils.write_json_file(outfile, res)
--- driver.sh
+++ driver.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ./gen_coord.py bangalore.coord.range 100 resources/tmpfiles/co_ords  && \
-./assign_vol.py resources/tmpfiles/co_ords 30.5 20 resources/tmpfiles/locs  && \
+./assign_vol.py resources/tmpfiles/co_ords 30.5 2016-03-12 60 resources/tmpfiles/locs  && \
 ./kmeans.py 15 resources/tmpfiles/locs resources/tmpfiles/clusters  && \
 ./initial_route.py resources/tmpfiles/clusters 2 23000 4 resources/tmpfiles/initial_route  && \
 ./algorithm_route.py resources/tmpfiles/clusters resources/tmpfiles/initial_route 30 resources/tmpfiles/final_route 
