4c4851df7525c8e87ae4f2cdb0a55413ef05f02b
Vivek Bharadwaj <bvivek35@gmail.com>
2016-05-10 10:57:57 +0530

    added specific fns. Need to modify components to use them

--- DBLayer.py
+++ DBLayer.py
@@ -1,7 +1,41 @@
 import pymongo
-MONGO_HOST = "localhost"
-MONGO_PORT = 27017
 
+from DBConfig import *
+
+def write_co_ords(docs) :
+    return insert_docs(CO_ORDS_DB, CO_ORDS_COLL, docs)
+    
+def read_co_ords() :
+    return get_docs(CO_ORDS_DB, CO_ORDS_COLL, docs)
+
+def write_locs(docs) :
+    return insert_docs(LOCS_DB, LOCS_COLL, docs)
+
+def read_locs() :
+    return get_docs(LOCS_DB, LOCS_COLL, docs)
+    
+def write_clusters(docs) :
+    return insert_docs(CLUSTERS_DB, CLUSTERS_COLL, docs)
+
+def read_clusters() :
+    return get_docs(CLUSTERS_DB, CLUSTERS_COLL, docs)
+
+def write_initial_route(docs) :
+    return insert_docs(INITIAL_ROUTE_DB, INITAL_ROUTE_COLL, docs)
+
+def read_initial_route() :
+    return get_docs(INITIAL_ROUTE_DB, INITAL_ROUTE_COLL, docs)
+
+def write_final_route(docs) :
+    return insert_docs(FINAL_ROUTE_DB, FINAL_ROUTE_COLL, docs)
+
+def read_final_route() :
+    return get_docs(FINAL_ROUTE_DB, FINAL_ROUTE_COLL, docs)
+
+
+"""
+    Generic Fns To Interact With Mongo
+"""
 def insert_doc(db_, coll_, doc_) :
     client = pymongo.MongoClient(MONGO_HOST, MONGO_PORT)
     db = client[str(db_)]
@@ -9,7 +43,6 @@ def insert_doc(db_, coll_, doc_) :
     res = coll.insert_one(doc_)
     client.close()
     return res
-
 
 def insert_docs(db_, coll_, docs_) :
     client = pymongo.MongoClient(MONGO_HOST, MONGO_PORT)
@@ -26,4 +59,11 @@ def get_docs(db_, coll_) :
     res = list(coll.find())
     client.close()
     return res
-    
+
+def remove_docs(db_, coll_) :
+    client = pymongo.MongoClient(MONGO_HOST, MONGO_PORT)
+    db = client[str(db_)]
+    coll = db[str(coll_)]
+    res = coll.remove({})
+    client.close()
+    return res
